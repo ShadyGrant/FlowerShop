4c8236599c6b98460f3bdb92e298dff30401d10c
Shady Grant <shadygrant@shadys-mbp.lan>
2020-04-09 11:57:19 -0500

    added distributors

--- scripts/retailers/Retailer.js
+++ scripts/retailers/Retailer.js
@@ -1,9 +1,10 @@
-export const RetailerHTML = (retailerObject) => {
+export const RetailerHTML = (retailerObject, distributorObject) => {
     return `
         <section class="retailer">
             <h2>Name: ${retailerObject.name}</h2>
             <div>City: ${retailerObject.city}</div>
             <div>State: ${retailerObject.state}</div>
+            <div>Distributor: ${distributorObject.name}</div>
         </section>
     `
 }
